10199ea7e78bf2a25400816cab6133b3a9519a15
Thijs Roem <thijsroem@gmail.com>
2019-02-26 09:54:10 +0100

    26_2_1

---
+++ BuildingPlayfulWorlds/Assets/Bridge_manager_02.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge_manager_02 : MonoBehaviour
+{
+    bool openBridge = false;
+    bool bridgeOpened = false;
+    public Flag[] flags;
+    public Transform bridge;
+    public FlagController flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flag = GameObject.FindGameObjectWithTag("Player").GetComponent<FlagController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (flag.hasFlag == true)
+            { 
+                Debug.Log("Flag added");
+            }
+            else
+            {
+                Debug.Log("Need Flag");
+            }
+        }
+    }
+}
---
+++ BuildingPlayfulWorlds/Assets/Scripts/FlagController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    public Transform flagPosition;
+
+    private Flag currentFlag = null;
+
+    public bool hasFlag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        //pick up the flag
+        Flag flag = col.gameObject.GetComponent<Flag>();
+        if (hasFlag == false)
+        {
+            if (flag != null)
+            {
+                currentFlag = flag;
+                currentFlag.OnPickup(flagPosition);
+                hasFlag = true;
+                Debug.Log("Picked up flag");
+            }
+        }
+        else
+        {
+            Debug.Log("Already has flag");
+        }
+    }
+}
