020d9dd171e94daebeb9fc89160b028f7e7581fd
Thijs Roem <thijsroem@gmail.com>
2019-03-04 15:49:43 +0100

    Update 0.03.01
    Puzzle 3 core added
    Post processing added

---
+++ BuildingPlayfulWorlds/Assets/DeathTrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrap : MonoBehaviour
+{
+    public bool isDangerous;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isDangerous == true)
+        {
+            //Destroy(player);
+            Debug.Log("You died!");
+        }
+    }
+}
