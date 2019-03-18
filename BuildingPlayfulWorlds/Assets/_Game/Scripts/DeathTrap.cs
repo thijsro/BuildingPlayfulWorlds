8e94e9c153fd4d847f6b7e58739427ba4953dae7
Thijs Roem <thijsroem@gmail.com>
2019-03-18 15:16:24 +0100

    Update 0.04.01 Island update
    More rocks
    More textures
    Deleted useless textures, materials and groundlayers

--- BuildingPlayfulWorlds/Assets/_Game/Scripts/DeathTrap.cs
+++ BuildingPlayfulWorlds/Assets/_Game/Scripts/DeathTrap.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class DeathTrap : MonoBehaviour
 {
     public bool isDangerous;
+    public bool isLastOne;
+    public bool isFirstOne;
+    Material DeathTrapMaterial;
+    public Transform deathTrap;
+    public Material DeathTrap_0;
+    public Material DeathTrap_1;
+    public Material DeathTrap_2;
+    public GameObject Wall;
+    DeathTrapManager Manager;
 
     GameObject player;
 
@@ -12,12 +21,14 @@ public class DeathTrap : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        DeathTrapMaterial = deathTrap.gameObject.GetComponent<Renderer>().sharedMaterial;
+        Manager = GetComponentInParent<DeathTrapManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
@@ -27,6 +38,30 @@ public class DeathTrap : MonoBehaviour
         {
             //Destroy(player);
             Debug.Log("You died!");
+            Manager.isResetting = true;
+        }
+        else
+        {
+            if (isFirstOne == true)
+            {
+                Manager.resetDone = true;
+
+            }
+            deathTrap.gameObject.GetComponent<Renderer>().sharedMaterial = DeathTrap_2;
+            if (isLastOne == true)
+            {
+                Debug.Log("Wall destroyed");
+                Destroy(Wall);
+            }
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isFirstOne == true)
+        {
+            Manager.resetDone = false;
+        }
+    }
+
 }
---
+++ BuildingPlayfulWorlds/Assets/_Game/Scripts/DeathTrapManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrapManager : MonoBehaviour
+{
+    DeathTrap[] DeathTraps;
+    public bool isResetting;
+    public bool resetDone;
+    public bool doOnce1;
+    public bool doOnce2;
+    public Material DeathTrap_0;
+    public Material DeathTrap_1;
+    public Material DeathTrap_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeathTraps = GetComponents<DeathTrap>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isResetting == true){
+            if (!doOnce1)
+            {
+                DeathPuzzle();
+                doOnce1 = true;
+            }
+
+        }
+        if (resetDone == true)
+        {
+            if (!doOnce2)
+            {
+                ResetPuzzle();
+                doOnce2 = true;
+            }
+
+        }
+    }
+    
+    void DeathPuzzle()
+    {
+        for (int i = 0; i < DeathTraps.Length; i++)
+        {
+            DeathTraps[i].gameObject.GetComponent<Renderer>().sharedMaterial = DeathTrap_1;
+        }
+        Debug.Log("deathtrap_1 new material");
+    }
+
+    void ResetPuzzle()
+    {
+        for (int i = 0; i < DeathTraps.Length; i++)
+        {
+            DeathTraps[i].gameObject.GetComponent<Renderer>().sharedMaterial = DeathTrap_0;
+        }
+
+    }
+}
